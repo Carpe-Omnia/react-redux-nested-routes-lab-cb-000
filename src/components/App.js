bdac3b307ded9e956715c6de8d297e9d81c7d62d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 21:05:12 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import PetsPage from '../containers/PetsPage';
+import PetsShow from '../containers/PetsShow';
 
 const App = (props) => 
   <Router>
@@ -11,6 +12,7 @@ const App = (props) =>
       </div>
       <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
       <Route path="/pets" component={PetsPage} />
+      <Route path="/pets/:petId" component={PetsShow} />
     </div>
   </Router>;
 
